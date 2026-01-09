73604cf7b387d090248c3ce0d3146a92eb06a0ce
korolev5526 <korolev5526@yandex.ru>
2026-01-09 18:32:12 +0700

    edit constructor in base_page class, add is_element_present method, add annotations

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,19 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
 class BasePage():
-  def __init__(self, browser, url):
+  def __init__(self, browser: webdriver.Chrome, url: str, timeout: int = 10) -> None:
     self.browser = browser
     self.url = url
+    self.browser.implicitly_wait(timeout)
 
-  def open(self):
+  def open(self) -> None:
     self.browser.get(self.url)
+
+  def is_element_present(self, how: By, what: str) -> bool:
+    try:
+      self.browser.find_element(how, what)
+    except NoSuchElementException:
+      return False
+    return True
