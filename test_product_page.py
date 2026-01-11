c2dbd338b4bc24b4d11bd321b4b0e79835682d3d
korolev5526 <korolev5526@yandex.ru>
2026-01-11 14:46:37 +0700

    add parametrize to test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,26 @@
 from .pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-  link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-  product_page = ProductPage(browser, link)
-  product_page.open()
-  product_page.add_product_to_busket()
-  product_page.solve_quiz_and_get_code()
-  product_page.check_added_to_busket_product_name()
-  product_page.check_added_to_busket_product_price()
+promo_links = [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+    pytest.param(
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+]
+
+
+@pytest.mark.parametrize('link', promo_links)
+def test_guest_can_add_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_busket()
+    product_page.solve_quiz_and_get_code()
+    product_page.check_added_to_busket_product_name()
+    product_page.check_added_to_busket_product_price()
